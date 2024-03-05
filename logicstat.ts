06f9e3cbd85b2aff1431882e0b1359685829b74a
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-05 10:31:36 +0500

    Add files via upload

---
+++ logicstat.ts
@@ -0,0 +1,26 @@
+console.log("Logic Statements-If, Else-if, Else");
+let sick=true;
+if (sick) {
+    console.log("Take medicines");
+}else{
+    console.log("Eat ice-cream")
+}
+console.log(sick)
+
+let sick2=false;
+if (sick2){
+    console.log("Eat Icecream");
+}else{
+    console.log("Take medicines")
+}
+console.log(sick2)
+
+let weather="cloudy";
+if (weather==="rainy"){
+    console.log("Wear a raincoat")
+}else if
+    (weather==="cloudy"){
+    console.log("Wear a light jacket") //true
+}else {
+    console.log("Wear sunglasses")
+}
